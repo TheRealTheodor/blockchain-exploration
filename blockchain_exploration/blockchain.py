83f94407370cd13a48eb8fb007557f5b35817df4
Matej Dvoracek <matej.dvoracek@eon.cz>
2025-03-19 22:05:43 +0100

    correction

--- blockchain_exploration/blockchain.py
+++ blockchain_exploration/blockchain.py
@@ -1,13 +1,8 @@
 import hashlib
 import json
-import sys
 
 
 def hash(message: str = "") -> str:
     if isinstance(message, str):
         message = json.dumps(message, sort_keys=True)
-
-    if sys.version_info.major == 2:
-        return hashlib.sha256(message).hexdigest()
-    else:
-        return hashlib.sha256(str(message).encode("utf-8")).hexdigest()
+    return hashlib.sha256(str(message).encode("utf-8")).hexdigest()
